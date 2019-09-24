1f0fc1eed8eded22da6e64b8a84ae03f18874b4b
shirokuro <46kuro212@gmail.com>
2019-09-24 13:37:12 +0900

    Fix leak of 'Override' annotation

--- src/main/java/com/github/kuro46/scriptblockimproved/script/option/placeholder/PlayerPlaceholder.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/option/placeholder/PlayerPlaceholder.java
@@ -5,10 +5,12 @@ import org.bukkit.entity.Player;
 
 public final class PlayerPlaceholder implements Placeholder {
 
+    @Override
     public PlaceholderName getName() {
         return PlaceholderName.of("player");
     }
 
+    @Override
     public String getReplaceTo(final Player player, final BlockCoordinate coordinate) {
         return player.getName();
     }
--- src/main/java/com/github/kuro46/scriptblockimproved/script/option/placeholder/WorldPlaceholder.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/option/placeholder/WorldPlaceholder.java
@@ -5,10 +5,12 @@ import org.bukkit.entity.Player;
 
 public final class WorldPlaceholder implements Placeholder {
 
+    @Override
     public PlaceholderName getName() {
         return PlaceholderName.of("world");
     }
 
+    @Override
     public String getReplaceTo(final Player player, final BlockCoordinate coordinate) {
         return coordinate.getWorld();
     }
