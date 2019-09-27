b178cc66ebd152abfc0cd03c35b52dd183518437
shirokuro <46kuro212@gmail.com>
2019-09-27 22:23:00 +0900

    Refactor Scripts a bit

--- src/main/java/com/github/kuro46/scriptblockimproved/script/Scripts.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/Scripts.java
@@ -5,37 +5,24 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import lombok.NonNull;
 
 public final class Scripts {
 
     private final List<ScriptsListener> listeners = new ArrayList<>();
-    private final ConcurrentMap<BlockPosition, List<Script>> scripts;
+    private final ConcurrentMap<BlockPosition, CopyOnWriteArrayList<Script>> scripts =
+        new ConcurrentHashMap<>();
 
-    public Scripts() {
-        this(Collections.emptyMap());
-    }
-
-    public Scripts(final Map<BlockPosition, List<Script>> scripts) {
-        Objects.requireNonNull(scripts, "'scripts' cannot be null");
-
-        this.scripts = new ConcurrentHashMap<>(scripts);
-    }
-
-    public void addListener(final ScriptsListener listener) {
+    public void addListener(@NonNull final ScriptsListener listener) {
         listeners.add(listener);
     }
 
-    public static Scripts fromJson(final JsonArray json) {
-        Objects.requireNonNull(json, "'json' cannot be null");
-
+    public static Scripts fromJson(@NonNull final JsonArray json) {
         final Scripts scripts = new Scripts();
         json.forEach(element -> scripts.add(Script.fromJson((JsonObject) element)));
         return scripts;
@@ -53,25 +40,20 @@ public final class Scripts {
         return scripts.keySet();
     }
 
-    public void add(final Script script) {
-        Objects.requireNonNull(script, "'script' cannot be null");
-
+    public void add(@NonNull final Script script) {
         final List<Script> scripts = this.scripts.computeIfAbsent(
             script.getPosition(),
-            position -> new CopyOnWriteArrayList<>());
+            p -> new CopyOnWriteArrayList<>());
         scripts.add(script);
         listeners.forEach(listener -> listener.onModified(this));
     }
 
-    public void removeAll(final BlockPosition position) {
-        Objects.requireNonNull(position, "'position' cannot be null");
-
+    public void removeAll(@NonNull final BlockPosition position) {
         if (!scripts.containsKey(position)) {
             throw new IllegalArgumentException(
                     String.format("Script not exists at '%s'", position));
         }
         scripts.remove(position);
-
         listeners.forEach(listener -> listener.onModified(this));
     }
 
@@ -79,8 +61,7 @@ public final class Scripts {
         return scripts.containsKey(position);
     }
 
-    public ImmutableList<Script> get(final BlockPosition position) {
-        Objects.requireNonNull(position, "'position' cannot be null");
+    public ImmutableList<Script> get(@NonNull final BlockPosition position) {
         return ImmutableList.copyOf(scripts.get(position));
     }
 }
