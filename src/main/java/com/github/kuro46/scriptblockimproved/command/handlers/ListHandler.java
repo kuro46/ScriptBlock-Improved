b4bdfe3bcc0ed4cc3feb9b022cde90e6ead521d0
shirokuro <46kuro212@gmail.com>
2019-10-02 21:02:35 +0900

    Send message when scripts are empty

--- src/main/java/com/github/kuro46/scriptblockimproved/command/handlers/ListHandler.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/command/handlers/ListHandler.java
@@ -1,5 +1,6 @@
 package com.github.kuro46.scriptblockimproved.command.handlers;
 
+import com.github.kuro46.scriptblockimproved.common.MessageKind;
 import com.github.kuro46.scriptblockimproved.common.command.Args;
 import com.github.kuro46.scriptblockimproved.common.command.CommandHandler;
 import com.github.kuro46.scriptblockimproved.common.command.ExecutionData;
@@ -27,6 +28,10 @@ public final class ListHandler extends CommandHandler {
         final CommandSender sender = data.getDispatcher();
 
         final List<BlockPosition> positions = new ArrayList<>(scripts.getPositions());
+        if (positions.isEmpty()) {
+            sendMessage(sender, MessageKind.ERROR, "Empty");
+            return;
+        }
         Collections.sort(positions);
         int count = 0;
         for (final BlockPosition position : positions) {
