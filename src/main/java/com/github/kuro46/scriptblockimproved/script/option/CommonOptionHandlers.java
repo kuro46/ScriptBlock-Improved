d9f4d76f4c8d94d2ae4995a962df455c4d1a913f
shirokuro <46kuro212@gmail.com>
2019-10-16 18:56:32 +0900

    Refactor CommonOptionHandlers

--- src/main/java/com/github/kuro46/scriptblockimproved/script/option/CommonOptionHandlers.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/option/CommonOptionHandlers.java
@@ -23,113 +23,49 @@ public final class CommonOptionHandlers {
         Objects.requireNonNull(handlers, "'handlers' cannot be null");
         Objects.requireNonNull(plugin, "'plugin' cannot be null");
 
-        handlers.add(OptionName.of("command"), COMMAND_HANDLER);
-        handlers.add(OptionName.of("console"), CONSOLE_HANDLER);
-        handlers.add(OptionName.of("broadcast"), BROADCAST_HANDLER);
-        handlers.add(OptionName.of("say"), SAY_HANDLER);
-        handlers.add(
-                OptionName.of("bypassCommand"),
-                new BypassCommandHandler(plugin));
+        OptionHandler.builder()
+            .args(Args.builder()
+                .required("command")
+                .build())
+            .executor(data -> {
+                final ParsedArgs args = data.getArgs();
+                final String command = removeSlashIfNeeded(args.getOrFail("command"));
+                data.getPlayer().performCommand(command);
+            })
+            .register(handlers, "command");
+        OptionHandler.builder()
+            .args(Args.builder()
+                .required("command")
+                .build())
+            .executor(data -> {
+                final ParsedArgs args = data.getArgs();
+                final String command = removeSlashIfNeeded(args.getOrFail("command"));
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+            })
+            .register(handlers, "console");
+        OptionHandler.builder()
+            .args(Args.builder()
+                .required("message")
+                .build())
+            .executor(data -> {
+                final ParsedArgs args = data.getArgs();
+                Bukkit.broadcastMessage(args.getOrFail("message"));
+            })
+            .register(handlers, "broadcast");
+        OptionHandler.builder()
+            .args(Args.builder()
+                .required("message")
+                .build())
+            .executor(data -> {
+                final ParsedArgs args = data.getArgs();
+                data.getPlayer().sendMessage(args.getOrFail("message"));
+            })
+            .register(handlers, "say");
+        handlers.add("bypassCommand", new BypassCommandHandler(plugin));
     }
 
-    private static final OptionHandler COMMAND_HANDLER = new OptionHandler() {
-
-        private final Args args = Args.builder()
-            .required("command")
-            .build();
-
-        @Override
-        public Args getArgs() {
-            return args;
-        }
-
-        @Override
-        public PreExecuteResult preExecute(final ExecutionData data) {
-            return PreExecuteResult.CONTINUE;
-        }
-
-        @Override
-        public void execute(final ExecutionData data) {
-            final ParsedArgs args = data.getArgs();
-            data.getPlayer().performCommand(removeSlashIfNeeded(args.getOrFail("command")));
-        }
-    };
-
-    private static final OptionHandler CONSOLE_HANDLER = new OptionHandler() {
-
-        private final Args args = Args.builder()
-            .required("command")
-            .build();
-
-        @Override
-        public Args getArgs() {
-            return args;
-        }
-
-        @Override
-        public PreExecuteResult preExecute(final ExecutionData data) {
-            return PreExecuteResult.CONTINUE;
-        }
-
-        @Override
-        public void execute(final ExecutionData data) {
-            final ParsedArgs args = data.getArgs();
-            Bukkit.dispatchCommand(
-                    Bukkit.getConsoleSender(),
-                    removeSlashIfNeeded(args.getOrFail("command")));
-        }
-    };
-
-    private static final OptionHandler BROADCAST_HANDLER = new OptionHandler() {
-
-        private final Args args = Args.builder()
-            .required("message")
-            .build();
-
-        @Override
-        public Args getArgs() {
-            return args;
-        }
-
-        @Override
-        public PreExecuteResult preExecute(final ExecutionData data) {
-            return PreExecuteResult.CONTINUE;
-        }
-
-        @Override
-        public void execute(final ExecutionData data) {
-            final ParsedArgs args = data.getArgs();
-            Bukkit.broadcastMessage(args.getOrFail("message"));
-        }
-    };
-
-    private static final OptionHandler SAY_HANDLER = new OptionHandler() {
-
-        private final Args args = Args.builder()
-            .required("message")
-            .build();
-
-        @Override
-        public Args getArgs() {
-            return args;
-        }
-
-        @Override
-        public PreExecuteResult preExecute(final ExecutionData data) {
-            return PreExecuteResult.CONTINUE;
-        }
-
-        @Override
-        public void execute(final ExecutionData data) {
-            final ParsedArgs args = data.getArgs();
-            data.getPlayer().sendMessage(args.getOrFail("message"));
-        }
-    };
-
     private static String removeSlashIfNeeded(final String source) {
-        if (source.isEmpty() || source.charAt(0) != '/') {
-            return source;
-        }
+        if (source.isEmpty() || source.charAt(0) != '/') return source;
         // 'source' is not empty and starts with '/'
         return source.substring(1);
     }
