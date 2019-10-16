3f83f4abfcd39ec26e052d1ecc1b418b26abf716
shirokuro <46kuro212@gmail.com>
2019-10-16 18:45:43 +0900

    Add builder for OptionHandler (#3)

--- src/main/java/com/github/kuro46/scriptblockimproved/script/option/OptionHandler.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/option/OptionHandler.java
@@ -1,6 +1,8 @@
 package com.github.kuro46.scriptblockimproved.script.option;
 
 import com.github.kuro46.scriptblockimproved.common.command.Args;
+import java.util.Objects;
+import lombok.NonNull;
 
 public interface OptionHandler {
 
@@ -9,4 +11,83 @@ public interface OptionHandler {
     PreExecuteResult preExecute(final ExecutionData data);
 
     void execute(final ExecutionData data);
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Functional interface for Builder
+     */
+    @FunctionalInterface
+    public interface PreExecutor {
+
+        PreExecuteResult execute(final ExecutionData data);
+    }
+
+    /**
+     * Functional interface for Builder
+     */
+    @FunctionalInterface
+    public interface Executor {
+
+        void execute(final ExecutionData data);
+    }
+
+    public static final class Builder {
+
+        private PreExecutor preExecutor = data -> PreExecuteResult.CONTINUE;
+        private Executor executor;
+        private Args args;
+
+        public Builder args(final Args args) {
+            this.args = args;
+            return this;
+        }
+
+        public Builder preExecutor(final PreExecutor preExecutor) {
+            this.preExecutor = preExecutor;
+            return this;
+        }
+
+        public Builder executor(final Executor executor) {
+            this.executor = executor;
+            return this;
+        }
+
+        public OptionHandler build() {
+            Objects.requireNonNull(args, "'args' cannot be null");
+            Objects.requireNonNull(preExecutor, "'preExecutor' cannot be null");
+            Objects.requireNonNull(executor, "'executor' cannot be null");
+            return new OptionHandler() {
+
+                @Override
+                public Args getArgs() {
+                    return args;
+                }
+
+                @Override
+                public PreExecuteResult preExecute(final ExecutionData data) {
+                    return preExecutor.execute(data);
+                }
+
+                @Override
+                public void execute(final ExecutionData data) {
+                    executor.execute(data);
+                }
+            };
+        }
+
+        public void register(
+                @NonNull final OptionHandlers handlers,
+                @NonNull final OptionName name) {
+            handlers.add(name, build());
+        }
+
+        public void register(
+                @NonNull final OptionHandlers handlers,
+                @NonNull final String name) {
+            register(handlers, OptionName.of(name));
+        }
+    }
 }
--- src/main/java/com/github/kuro46/scriptblockimproved/script/option/OptionHandlers.java
+++ src/main/java/com/github/kuro46/scriptblockimproved/script/option/OptionHandlers.java
@@ -3,8 +3,8 @@ package com.github.kuro46.scriptblockimproved.script.option;
 import com.google.common.collect.ImmutableSet;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
+import lombok.NonNull;
 
 public final class OptionHandlers {
 
@@ -14,33 +14,25 @@ public final class OptionHandlers {
         return ImmutableSet.copyOf(handlers.keySet());
     }
 
-    public boolean contains(final OptionName key) {
-        Objects.requireNonNull(key, "'key' cannot be null");
-
+    public boolean contains(@NonNull final OptionName key) {
         return handlers.containsKey(key);
     }
 
-    public void add(final OptionName key, final OptionHandler handler) {
-        Objects.requireNonNull(key, "'key' cannot be null");
-        Objects.requireNonNull(handler, "'handler' cannot be null");
-
+    public void add(
+            @NonNull final OptionName key,
+            @NonNull final OptionHandler handler) {
         if (contains(key)) {
             throw new IllegalArgumentException(
                     String.format("A handler for '%s' has already added", key));
         }
-
         handlers.put(key, handler);
     }
 
-    public Optional<OptionHandler> get(final OptionName key) {
-        Objects.requireNonNull(key, "'key' cannot be null");
-
+    public Optional<OptionHandler> get(@NonNull final OptionName key) {
         return Optional.ofNullable(handlers.get(key));
     }
 
-    public OptionHandler getOrFail(final OptionName key) {
-        Objects.requireNonNull(key, "'key' cannot be null");
-
+    public OptionHandler getOrFail(@NonNull final OptionName key) {
         return get(key).orElseThrow(() -> new IllegalArgumentException(
                 String.format("A handler for '%s' not exists", key)));
     }
